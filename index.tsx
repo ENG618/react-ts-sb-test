a05655b83b901e3cc2707a582d3529d9c9dac1f7
eng618 <engarcia618@gmail.com>
2023-01-06 10:37:33 -0500

    style: format files

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
@@ -12,7 +12,7 @@ class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: 'React',
     };
   }
 
@@ -20,12 +20,8 @@ class App extends Component<AppProps, AppState> {
     return (
       <div>
         <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-        <p>
-          From Eric Garcia!
-        </p>
+        <p>Start editing to see some magic happen :)</p>
+        <p>From Eric Garcia!</p>
         <Hello name="Eric" />
       </div>
     );
